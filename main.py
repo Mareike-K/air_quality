1e8d24f555e00646e8e1555d1897f92d34b5a936
Mareike Keller <mareike_keller@hotmail.com>
2025-04-24 16:11:01 +0200

    update

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# File not in use yet!
+# This is a placeholder for the main entry point of the air-quality pipeline.
+# It will be used to run the pipeline and orchestrate the different components.
+
+
 def main():
     print("Hello from air-quality!")
 
